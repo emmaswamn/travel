32d21199935aac3dc85d6dd43ede16b0948f5237
emmaswamn <87064359+emmaswamn@users.noreply.github.com>
2022-10-21 21:12:46 +0800

    Add reccomend view.

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -2,6 +2,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
     <div>Home page</div>
 </template>
 
@@ -9,4 +10,5 @@
 import HomeHeader from './component/HomeHeader.vue'
 import HomeSwiper from './component/HomeSwiper.vue'
 import HomeIcons from './component/HomeIcons.vue'
+import HomeRecommend from './component/HomeRecommend.vue'
 </script>
