6c05fc2f95cd258713658f5879f2dc43786af9dc
emmaswamn <87064359+emmaswamn@users.noreply.github.com>
2022-10-21 20:32:46 +0800

    Add home icons

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
     <div>Home page</div>
 </template>
 
 <script setup>
 import HomeHeader from './component/HomeHeader.vue'
 import HomeSwiper from './component/HomeSwiper.vue'
+import HomeIcons from './component/HomeIcons.vue'
 </script>
