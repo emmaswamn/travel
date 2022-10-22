da95472115c0ed03931f08938037189371902422
emmaswamn <87064359+emmaswamn@users.noreply.github.com>
2022-10-22 21:28:46 +0800

    Add netlify functions.

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -3,7 +3,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
-    <div>Home page</div>
+    <div>{{result}}</div>
 </template>
 
 <script setup>
@@ -11,4 +11,19 @@ import HomeHeader from './component/HomeHeader.vue'
 import HomeSwiper from './component/HomeSwiper.vue'
 import HomeIcons from './component/HomeIcons.vue'
 import HomeRecommend from './component/HomeRecommend.vue'
+import axios from 'axios';
+import { onMounted } from 'vue'
+
+const fetchData = async () => {
+    try {
+        const {data} = await axios.get('/api/hello');
+        const result = data;
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+onMounted(() => {
+    fetchData();
+})
 </script>
