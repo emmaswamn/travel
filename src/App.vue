d7b7334d3c7067422e9e9c4b37d5e951cefe9134
emmaswamn <87064359+emmaswamn@users.noreply.github.com>
2022-10-25 13:51:40 +0800

    Add detail banner.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view  v-slot="{Component}">
-    <keep-alive>
+    <keep-alive exclude="DetailView">
       <component :is="Component" :key="$route.fullPath"/>
     </keep-alive>
   </router-view>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter,createWebHistory } from 'vue-router';
 import HomeView from './pages/home/HomeView.vue';
 import CityView from './pages/city/CityView.vue'
+import DetailView from './pages/detail/DetailView.vue'
 
 const router = createRouter({
     history:createWebHistory(),
     routes: [
         {path: '/', component: HomeView},
-        {path: '/city', component: CityView}
+        {path: '/city', component: CityView},
+        {path: '/detail/:id', component: DetailView},
     ]
 });
 
